5a5686b7b33b72a07a99684448f3617349eb6c18
Abdeldciss <a_kellal@hotmail.fr>
2016-12-12 16:33:04 +0100

    add initial commit
    add coordonnee class

---
+++ src/batailleNavale/Navire.java
@@ -0,0 +1,47 @@
+package batailleNavale;
+
+public class Navire {
+
+	
+	private Coordonnee debut;
+	private Coordonnee fin;
+	private Coordonnee[] partiesTouchees;
+	private int nbTouchees;
+	
+	
+	public Navire(Coordonnee debut, int longueur, boolean estVertical) {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
