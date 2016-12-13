220212f20797650ec23bc4b88d4746eea1a9aea6
Abdeldciss <a_kellal@hotmail.fr>
2016-12-13 09:05:26 +0100

    add class navire

--- src/batailleNavale/Navire.java
+++ src/batailleNavale/Navire.java
@@ -1,5 +1,5 @@
 package batailleNavale;
-
+//import batailleNavale.Coordonnee.java;
 public class Navire {
 
 	
@@ -10,37 +10,67 @@ public class Navire {
 	
 	
 	public Navire(Coordonnee debut, int longueur, boolean estVertical) {
-		
-		
-		
+		this.debut=debut;
+		estVertical=false;
+		boolean horizontal=false;
+		if(fin.getColonne()==debut.getColonne()){
+			estVertical=true;
+			longueur=fin.getLigne()+debut.getLigne();
+		}else if(fin.getLigne()==debut.getLigne()){
+			estVertical=false;
+			horizontal=true;
+			longueur=fin.getColonne()+debut.getColonne();
+		}
+		nbTouchees=0;
+		partiesTouchees=new Coordonnee[0]; 
 	}
 	
 	
+	public Coordonnee getDebut() {
+		return debut;
+	}
+
+
+	public Coordonnee getFin() {
+		return fin;
+	}
+
+	public String toString(){
+		
+		String s="";
+		
+		return s+"Navire("+this+")";
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Navire n=new Navire("B1", 4, horizontal);
 
 	}
 
